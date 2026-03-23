665fb53488dbf065c50f42af5b598d0daa0ec199
kiratekki <kiratekki1099@gmail.com>
2026-03-24 10:54:47 +1300

    Configure Bandit to handle known accepted risks

--- app.py
+++ app.py
@@ -14,19 +14,19 @@ def login():
 		password = request.form['password']
 		if username == susername  and password == spassword: 
 			return redirect(url_for('home')) 
-		else: 
+		else:
 			return(" Credentials mismatch ")
-	return''' 
+	return'''
     		<form method="POST" action="/login">
     		<input type="text" name="username" placeholder="Username"><br>
     		<input type="password" name="password" placeholder="Password"><br>
     		<input type="submit" value="Login">
 		</form>
 		'''
-@app.route('/home', methods=['GET']) 
-def home(): 
+@app.route('/home', methods=['GET'])
+def home():
    return '''
       <H1> "Welcome to the Homepage" </H1> '''
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')   # nosec B104
